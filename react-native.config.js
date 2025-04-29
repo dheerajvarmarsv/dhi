cdc38edf057a49daf5b7a7214ae38f37c8cfe982
dheerajvarmarsv <dheeraj.rudraraju@cloudfabrix.com>
2025-04-29 09:59:07 -0700

    99% done with dhi

---
+++ react-native.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Currently it's not possible to customize the project name here,
+  // so we'll manually define the path to use
+  reactNativePath: './node_modules/react-native',
+  dependencies: {},
+}; 
